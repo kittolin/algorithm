752450dfd3bb310f45530089d4a1c7a209622065
Kitto <kittolin42@gmail.com>
2022-04-09 23:46:14 +0800

    Factorial

---
+++ recursion/factorial.cpp
@@ -0,0 +1,27 @@
+/*
+* 名称：n的阶乘
+* 来源：清华大学复试上机题
+* 链接：http://t.cn/Ai0ocOUY
+* 难度：简单
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+// !!! 阶乘结果非常大，需要用 long long
+long long Factorial(int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * Factorial(n - 1);
+}
+
+int main() {
+    int n;
+    while (scanf("%d",&n) != EOF) {
+        printf("%lld\n", Factorial(n));
+    }
+    return 0;
+}

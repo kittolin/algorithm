4a7a0930678675d1c07b3a3df83fa6e62a276f90
Kitto <kittolin42@gmail.com>
2022-04-10 17:42:32 +0800

    Binary Tree Nodes Num

---
+++ divide-and-conquer/binary_tree_nodes_num.cpp
@@ -0,0 +1,34 @@
+/*
+* 名称：二叉树
+* 来源：北京大学复试上机题
+* 链接：http://t.cn/Ai0Ke6I0
+* 难度：中等
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+/*
+*     i
+*    / \
+*  2i  2i+1
+*/
+int BinaryTreeNodesNum(int m, int n) {
+    if (m > n) {
+        return 0;
+    }
+    return 1 + BinaryTreeNodesNum(m * 2, n) + BinaryTreeNodesNum(m * 2 + 1, n);
+}
+
+int main() {
+    int m, n;
+    while (scanf("%d %d", &m, &n) != EOF) {
+        if (m == 0 && n == 0) {
+            break;
+        }
+        printf("%d\n", BinaryTreeNodesNum(m, n));
+    }
+    return 0;
+}

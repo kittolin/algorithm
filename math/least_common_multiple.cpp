59e1dde8a2ecd109418d660b540d01b23ce69f6a
Kitto <kittolin42@gmail.com>
2022-04-12 21:14:17 +0800

    Least Common Multiple

---
+++ math/least_common_multiple.cpp
@@ -0,0 +1,32 @@
+/*
+* 名称：最小公倍数(LCM)
+* 来源：杭电 ACM
+* 链接：https://acm.hdu.edu.cn/showproblem.php?pid=1108
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+// 辗转相除法(欧几里得算法)
+int GCD(int a, int b) {
+    while (a % b) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+    return b;
+}
+
+int LCM(int a, int b) {
+    return a * b / GCD(a, b);
+}
+
+int main() {
+    int a, b;
+    while (scanf("%d %d", &a, &b) != EOF) {
+        printf("%d\n", LCM(a, b));
+    }
+    return 0;
+}

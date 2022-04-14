f58121530fefed4587b463425e7943be7eb3e950
Kitto <kittolin42@gmail.com>
2022-04-14 11:08:33 +0800

    Zero Complexity Transposition

---
+++ stack/zero_complexity_transposition.cpp
@@ -0,0 +1,30 @@
+/*
+* 名称：Zero-complexity Transposition
+* 来源：上海交通大学复试上机题
+* 链接：http://t.cn/AiKa20bt
+* 难度：简单
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        stack<long long> s;
+        while (n --) {
+            long long a;
+            scanf("%lld", &a);
+            s.push(a);
+        }
+        while (!s.empty()) {
+            printf("%lld ", s.top());
+            s.pop();
+        }
+        printf("\n");
+    }
+    return 0;
+}

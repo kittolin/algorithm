e188f9881b79768b33a55fea41fa2a3fbcff4421
Kitto <kittolin42@gmail.com>
2022-04-14 23:40:00 +0800

    Reversal Ordinal

---
+++ enum/reversal_ordinal.cpp
@@ -0,0 +1,29 @@
+/*
+* 名称：反序数
+* 来源：清华大学复试上机题
+* 链接：http://t.cn/E9WBrut
+* 难度：中等
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int ReversalOrdinal(int number) {
+    int ans = 0;
+    while (number) {
+        ans = ans * 10 + number % 10;
+        number /= 10;
+    }
+    return ans;
+}
+
+int main() {
+    for (int n = 1000; n < 10000; n++) {
+        if (n * 9 == ReversalOrdinal(n)) {
+            printf("%d\n", n);
+        }
+    }
+    return 0;
+}

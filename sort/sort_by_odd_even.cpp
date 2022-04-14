8bb2cf781749590b0325439e2d5f260593dc42a1
Kitto <kittolin42@gmail.com>
2022-04-14 22:21:18 +0800

    Sort by Odd-Even

---
+++ sort/sort_by_odd_even.cpp
@@ -0,0 +1,40 @@
+/*
+* 名称：整数奇偶排序
+* 来源：北京大学复试上机题
+* 链接：http://t.cn/E9glPvp
+* 难度：中等 
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 10;
+int arr[N];
+
+// 若返回 true，则 x 排在 y 的前面
+bool Compare(int x, int y) {
+    if (x % 2 == 1 && y % 2== 1) {
+        return x > y;
+    }
+    if (x % 2 == 0 && y % 2 == 0) {
+        return x < y;
+    }
+    return x % 2 > y % 2;
+}
+
+int main() {
+    while (scanf("%d", &arr[0]) != EOF) {
+        for (int i = 1; i < N; i++) {
+            scanf("%d", &arr[i]);
+        }
+        sort(arr, arr + N, Compare);
+        for (int i = 0; i < N; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}

e55df4f074a2cd972b6d649b97d2311224494885
Kitto <kittolin42@gmail.com>
2022-04-15 01:22:31 +0800

    Output Trapezoid

---
+++ simulation/README.md
@@ -0,0 +1,3 @@
+## 输出梯形
+
+<img src="../images/simulation//output_trapezoid.png" width="300" height="300">
---
+++ simulation/output_trapezoid.cpp
@@ -0,0 +1,36 @@
+/*
+* 名称：输出梯形
+* 来源：清华大学复试上机题
+* 描述：输入一个高度 h(1 <= h <= 1000)，输出一个高度为 h，上底边为 h 的梯形。
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+// abase: 上底
+// h: 高
+// extra: 每行多几个字符
+void OutputTrapezoid(int abase, int h, int extra, char c) {
+    int ubase = abase + extra * (h - 1);    // 下底
+    for (int i = 0; i < h; i++) {
+        int ibase = abase + extra * i;
+        for (int j = 0; j < ubase; j++) {
+            if (j < ubase - ibase) {
+                printf(" ");
+            } else {
+                printf("%c", c);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int a, h, e;
+    while (cin >> a >> h >> e) {
+        OutputTrapezoid(a, h, e, '*');
+    }
+    return 0;
+}

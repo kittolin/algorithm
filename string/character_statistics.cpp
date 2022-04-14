d6a99217243a2e102d123eb2b5c75faa1ae06a88
Kitto <kittolin42@gmail.com>
2022-04-14 18:49:48 +0800

    Character Statistics

---
+++ string/character_statistics.cpp
@@ -0,0 +1,35 @@
+/*
+* 名称：统计字符
+* 来源：浙江大学复试上机题
+* 链接：http://t.cn/Ai8fvq4I
+* 难度：简单
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+void CharStatistics(string str1, string str2) {
+    unordered_map<char, int> map;
+    for (int i = 0; i < str2.size(); i++) {
+        map[str2[i]] ++;
+    }
+    for (int i = 0; i < str1.size(); i++) {
+        printf("%c %d\n", str1[i], map[str1[i]]);
+    }
+}
+
+int main() {
+    string str1, str2;
+    while (getline(cin, str1)) {
+        if (str1 == "#") {
+            break;
+        }
+        getline(cin, str2);
+        CharStatistics(str1, str2);
+    }
+    return 0;
+}

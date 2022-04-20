48eee9ce72adaa0f5e4ef077f352c10d27791340
Kitto <kittolin42@gmail.com>
2022-04-20 22:33:05 +0800

    Find Min Number Pair

---
+++ search/min_number_pari.cpp
@@ -0,0 +1,28 @@
+/*
+* 名称：找最小数
+* 来源：北京邮电大学复试上机题
+* 链接：http://985.so/9n8w
+* 难度：简单
+*/
+
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int n, x, y, minX, minY;
+    while (scanf("%d", &n) != EOF) {
+        scanf("%d %d", &minX, &minY);
+        for (int i = 1; i < n; i++) {
+            scanf("%d %d", &x, &y);
+            if (x < minX) {
+                minX = x;
+                minY = y;
+            }
+            if (x == minX && y < minY) {
+                minY = y;
+            }
+        }
+        printf("%d %d\n", minX, minY);
+    }
+}

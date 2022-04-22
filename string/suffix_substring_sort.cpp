150a31901c1fa16e1a1c5d3d73cff722f3bc0904
Kitto <kittolin42@gmail.com>
2022-04-22 21:35:02 +0800

    Suffix Substring Sort

---
+++ string/suffix_substring_sort.cpp
@@ -0,0 +1,28 @@
+/*
+* 名称：后缀子串排序
+* 来源：上海交通大学复试上机题
+* 链接：http://985.so/9z1g
+* 难度：入门
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string str;
+    while (cin >> str) {
+        int size = str.size();
+        string substrs[size];
+        for (int i = 0; i < size; i++) {
+            substrs[i] = str.substr(i);
+        }
+        sort(substrs, substrs + size);
+        for (int i = 0; i < size; i++) {
+            cout << substrs[i] << endl;
+        }
+    }
+    return 0;
+}

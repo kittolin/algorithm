74aa13ea8ce2dfb3deb442aa62ce17a871c80acb
Kitto <kittolin42@gmail.com>
2022-04-22 23:32:03 +0800

    Keyboard Error Decode

---
+++ simulation/keyboard_error_decode.cpp
@@ -0,0 +1,35 @@
+/*
+* 名称：键盘错误输入的字符串解码
+* 来源：上海交通大学复试上机题
+* 链接：http://985.so/90ay
+* 难度：简单
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+unordered_map<char, char> keyMap;
+
+void InitialKeyMap() {
+    string keys = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"; // '\' 斜杆字符需要转义
+    for (int i = 1; i < keys.size(); i++) {
+        keyMap[keys[i]] = keys[i - 1];
+    }
+}
+
+int main() {
+    InitialKeyMap();
+    string input;
+    while (getline(cin, input)) {
+        for (int i = 0; i < input.size(); i++) {
+            if (input[i] != ' ') {
+                input[i] = keyMap[input[i]];
+            }
+        }
+        cout << input << endl;
+    }
+    return 0;
+}

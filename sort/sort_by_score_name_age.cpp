6d22844aaac2f7be1d5ba582e16dcedbc9eb3ef4
Kitto <kittolin42@gmail.com>
2022-04-24 00:02:55 +0800

    Sort by Score Name Age

---
+++ sort/sort_by_score_name_age.cpp
@@ -0,0 +1,44 @@
+/*
+* 名称：成绩排序
+* 来源：华中科技大学复试上机题
+* 链接：http://985.so/96xn
+* 难度：较难
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 1000 + 10;
+
+struct Student {
+    string name;
+    int age;
+    int score;
+    bool operator<(const Student& s) const {
+        if (score != s.score) {
+            return score < s.score;
+        }
+        if (name != s.name) {
+            return name < s.name;
+        }
+        return age < s.age;
+    }
+};
+Student stus[MAXN];
+
+int main() {
+    int n;
+    while (cin >> n) {
+        for (int i = 0; i < n; i++) {
+            cin >> stus[i].name >> stus[i].age >> stus[i].score;
+        }
+        sort(stus, stus + n);
+        for (int i = 0; i < n; i++) {
+            cout << stus[i].name << " " << stus[i].age << " " << stus[i].score << endl;
+        }
+    }
+    return 0;
+}

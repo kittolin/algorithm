9da1fda61887bec199e3c1145c4a9c34ca3b6efe
Kitto <kittolin42@gmail.com>
2022-04-23 21:36:07 +0800

    Palindrome String

---
+++ string/palindrome_string.cpp
@@ -0,0 +1,33 @@
+/*
+* 名称：回文字符串
+* 来源：华中科技大学复试上机题
+* 链接：http://985.so/9vz4
+* 难度：较难
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IsPalindrome(string str) {
+    int i = 0, j = str.size() - 1;
+    while (i < j) {
+        if (str[i++] != str[j--]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string str;
+    while(getline(cin, str)) {
+        if (IsPalindrome(str)) {
+            printf("Yes!\n");
+        } else {
+            printf("No!\n");
+        }
+    }
+    return 0;
+}

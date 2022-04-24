bd2fbe4ebb5a24911d941e9567827a727edab926
Kitto <kittolin42@gmail.com>
2022-04-24 18:06:19 +0800

    The Number of Students With The Same Score

---
+++ hash/students_with_same_score.cpp
@@ -0,0 +1,29 @@
+/*
+* 名称：统计同成绩学生人数
+* 来源：浙江大学复试上机题
+* 链接：http://985.so/km2m
+* 难度：简单
+*/
+
+#include <cstdio>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    int n, score;
+    while (scanf("%d", &n) != EOF) {
+        if (n == 0) {
+            break;
+        }
+        unordered_map<int, int> map;
+        while (n --) {
+            int tmp;
+            scanf("%d", &tmp);
+            map[tmp] ++;
+        }
+        scanf("%d", &score);
+        printf("%d\n", map[score]);
+    }
+    return 0;
+}

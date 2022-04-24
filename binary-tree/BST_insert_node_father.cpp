a1622ae5945fcd4ea319c4f0c69009e7e8922b67
Kitto <kittolin42@gmail.com>
2022-04-24 12:21:05 +0800

    BST Insert Node Father

---
+++ binary-tree/BST_insert_node_father.cpp
@@ -0,0 +1,45 @@
+/*
+* 名称：二叉排序树插入节点，返回其父亲节点
+* 来源：华中科技大学复试上机题
+* 链接：http://985.so/9cmi
+* 难度：中等
+*/
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+struct BSTNode {
+    int data;
+    BSTNode* left;
+    BSTNode* right;
+    BSTNode(int d): data(d), left(nullptr), right(nullptr) {}
+};
+
+BSTNode* Insert(BSTNode* root, int data, int father) { // father: 当前 root 节点的父亲节点数值
+    if (root == nullptr) {
+        printf("%d\n", father);
+        return new BSTNode(data);
+    }
+
+    if (data > root->data) {
+        root->right = Insert(root->right, data, root->data);
+    } else if (data < root->data) {
+        root->left = Insert(root->left, data, root->data);
+    }
+    return root;
+}
+
+int main() {
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        BSTNode* root = nullptr;
+        while (n --) {
+            int data;
+            scanf("%d", &data);
+            root = Insert(root, data, -1);
+        }
+    }
+    return 0;
+}

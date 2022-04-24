c792a25ec6239ef3fda24a3ee1ceee0b45db8fe9
Kitto <kittolin42@gmail.com>
2022-04-24 18:17:45 +0800

    Collatz Conjecture

---
+++ math/Collatz_conjecture.cpp
@@ -0,0 +1,27 @@
+/*
+* 名称：考拉兹猜想
+* 来源：浙江大学复试上机题
+* 链接：http://985.so/kmk2
+* 难度：入门
+*/
+
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        int count = 0;
+        while (n != 1) {
+            if (n % 2) {
+                n = (3 * n + 1) / 2;
+            } else {
+                n /= 2;
+            }
+            count ++;
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}

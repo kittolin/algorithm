faefd7bfdf28c33d528ed7f2ad3f97490d0a7ef1
Kitto <kittolin42@gmail.com>
2022-04-27 22:09:00 +0800

    Power of numbers Irrelevant to Seven

---
+++ simulation/pow_of_number_irrelevant_to_seven.cpp
@@ -0,0 +1,52 @@
+/*
+* 名称：与 7 无关的数
+* 来源：北京大学复试上机题
+* 链接：http://985.so/k44a
+* 难度：中等
+*/
+
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+const int MAXN = 100;
+vector<int> numbers;
+
+bool Relevant2Seven(int number) {
+    if (number % 7 == 0) {
+        return true;
+    }
+    while (number) {
+        if (number % 10 == 7) {
+            return true;
+        }
+        number /= 10;
+    }
+    return false;
+}
+
+void Initial() {
+    for (int i = 1; i <= MAXN; i++) {
+        if (!Relevant2Seven(i)) {
+            numbers.push_back(i);
+        }
+    }
+}
+
+int main() {
+    Initial();
+    int n;
+    while (scanf("%d", &n) != EOF) {
+        int sum = 0;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers[i] <= n) {
+                sum += numbers[i] * numbers[i];
+            } else {
+                break;
+            }
+        }
+        printf("%d\n", sum);
+    }
+    return 0;
+}

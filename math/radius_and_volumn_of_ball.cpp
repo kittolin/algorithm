5be7405fa6210db65c24c8185fdfc3cf729b439b
Kitto <kittolin42@gmail.com>
2022-04-28 00:28:07 +0800

    Radius and Volume of Ball

---
+++ math/radius_and_volumn_of_ball.cpp
@@ -0,0 +1,23 @@
+/*
+* 名称：球的半径和体积
+* 来源：北京大学复试上机题
+* 链接：http://985.so/k47y
+* 难度：较难
+*/
+
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+const double PI = acos(-1);
+
+int main() {
+    double x0, y0, z0, x1, y1, z1, radius, volume;
+    while (scanf("%lf %lf %lf %lf %lf %lf" , &x0, &y0, &z0, &x1, &y1, &z1) != EOF) {
+        radius = sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0) + (z1 - z0) * (z1 - z0));
+        volume = radius * radius * radius * PI * 4 / 3;
+        printf("%.3f %.3f", radius, volume);
+    }
+    return 0;
+}

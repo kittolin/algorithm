452036016f0f8e64fac4cb4c3cd52bf72c3aa173
Kitto <kittolin42@gmail.com>
2022-04-28 01:24:12 +0800

    Number of Potential Friends

---
+++ array/number_of_potential_friends.cpp
@@ -0,0 +1,37 @@
+/*
+* 名称：谁是你的潜在朋友
+* 来源：北京大学复试上机题
+* 链接：http://985.so/k4f8
+* 难度：中等
+*/
+
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+const int MAXN = 200 + 10;
+const int MAXM = 200 + 10;
+
+int bookOfReader[MAXN];     // 每个读者喜欢的图书编号
+int readersOfBook[MAXM];    // 每一本图书被多少读者喜欢
+
+int main() {
+    int n, m, bookNumber;
+    while (scanf("%d %d", &n, &m) != EOF) {
+        memset(readersOfBook, 0, sizeof(readersOfBook));
+        for (int i = 1; i <= n; i++) {
+            scanf("%d", &bookNumber);
+            bookOfReader[i] = bookNumber;
+            readersOfBook[bookNumber] ++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (readersOfBook[bookOfReader[i]] > 1) {
+                printf("%d\n", readersOfBook[bookOfReader[i]] - 1);
+            } else {
+                printf("BeiJu\n");
+            }
+        }
+    }
+    return 0;
+}

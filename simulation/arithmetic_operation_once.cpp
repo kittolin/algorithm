f6cabd70057881969f8d0bacdf68762c85efd304
Kitto <kittolin42@gmail.com>
2022-04-27 19:13:12 +0800

    Arithmetic Operation Once

---
+++ simulation/arithmetic_operation_once.cpp
@@ -0,0 +1,52 @@
+/*
+* 名称：进行一次算术运算
+* 来源：北京大学复试上机题
+* 链接：http://985.so/k4bm
+* 难度：简单
+*/
+
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int Operation(int a, int b, char c) {
+    int result = 1;
+    if (c == '+') {
+        result = a + b;
+    } else if (c == '-') {
+        result = a - b;
+    } else if (c == '*') {
+        result = a * b;
+    } else if (c == '/') {
+        result = a / b;
+    } else if (c == '%') {
+        result = a % b;
+    } else {
+        for (int i = 2; i <= a; i++) {
+            result *= i;
+        }
+    }
+    return result;
+}
+
+int main() {
+    string expr;
+    int a, b;
+    char c;
+    while (getline(cin, expr)) {
+        istringstream steam(expr);
+        steam >> a >> c;
+        if (c != '!') {
+            steam >> b;
+        }
+
+        if ((c == '/' || c == '%') && b == 0) {
+            printf("error\n");
+        } else {
+            printf("%d\n", Operation(a, b, c));
+        }
+    }
+    return 0;
+}

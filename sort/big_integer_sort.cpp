0ae75888cc60bf2fb9567be758fbb2e94a6509e3
Kitto <kittolin42@gmail.com>
2022-04-30 23:08:50 +0800

    Big Integer Sort

---
+++ sort/big_integer_sort.cpp
@@ -0,0 +1,37 @@
+/*
+* 名称：大整数排序
+* 来源：华中科技大学复试上机题
+* 链接：http://985.so/kt2j
+* 难度：困难
+* 分析：用字符串代替大整数
+*/
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 100 + 10;
+string arr[MAXN];
+
+bool Compare(string str1, string str2) {
+    if (str1.size() == str2.size()) {
+        return str1 < str2;
+    }
+    return str1.size() < str2.size();
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n, Compare);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << endl;
+        }
+    }
+    return 0;
+}

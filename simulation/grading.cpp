e6159cff303d5ccbdac365667bdfb13d38473115
Kitto <kittolin42@gmail.com>
2022-05-01 18:54:42 +0800

    Grading

---
+++ simulation/grading.cpp
@@ -0,0 +1,38 @@
+/*
+* 名称：Grading
+* 来源：浙江大学复试上机题
+* 链接：http://985.so/k0ag
+* 难度：中等
+*/
+
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int Abs(int num) {
+    return num >= 0? num: -num;
+}
+
+double FinalGrade(int T, int G1, int G2, int G3, int GJ) {
+    if (Abs(G1 - G2) <= T) {
+        return (G1 + G2) / 2.0; 
+    }
+    if (Abs(G3 - G1) <= T && Abs(G3 - G2) <= T) {
+        return max(G1, G2, G3);
+    }
+    if (Abs(G3 - G1) <= T) {
+        return (G1 + G3) / 2.0;
+    }
+    if (Abs(G3 - G2) <= T) {
+        return (G2 + G3) / 2.0;
+    }
+    return GJ;
+}
+
+int main() {
+    int P, T, G1, G2, G3, GJ;
+    while (scanf("%d %d %d %d %d %d", &P, &T, &G1, &G2, &G3, &GJ) != EOF) {
+        printf("%.1f\n", FinalGrade(T, G1, G2, G3, GJ));
+    }
+}

039e62ea666176ae8b1f183e3bdfcbf113e6c867
Kitto <kittolin42@gmail.com>
2022-05-01 23:41:41 +0800

    Door Opener and Closer

---
+++ search/door_opener_and_closer.cpp
@@ -0,0 +1,32 @@
+/*
+* 名称：开门人和关门人
+* 来源：浙江大学复试上机题
+* 链接：http://985.so/k0z6
+* 难度：中等
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int m;
+    string number, checkIn, checkOut;
+    while (cin >> m) {
+        string opener = "", closer = "", earliestCheckIn = "23:59:59", latestCheckOut = "00:00:00";
+        while (m --) {
+            cin >> number >> checkIn >> checkOut;
+            if (checkIn <= earliestCheckIn) {
+                opener = number;
+                earliestCheckIn = checkIn;
+            }
+            if (checkOut >= latestCheckOut) {
+                closer = number;
+                latestCheckOut = checkOut;
+            }
+        }
+        cout << opener << " " << closer << endl;
+    }
+    return 0;
+}

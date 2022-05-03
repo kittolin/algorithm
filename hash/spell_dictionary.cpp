edd11bf81a66b2f5c0b0f64ecd10e92249440df5
Kitto <kittolin42@gmail.com>
2022-05-03 19:18:56 +0800

    Spell Dictionary

---
+++ hash/spell_dictionary.cpp
@@ -0,0 +1,41 @@
+/*
+* 名称：魔咒词典
+* 来源：浙江大学复试上机题
+* 链接：http://985.so/k16x
+* 难度：中等
+*/
+
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    string item, spell, function;
+    unordered_map<string, string> map;
+    while (getline(cin, item)) {
+        if (item == "@END@") {
+            break;
+        }
+        spell = item.substr(0, item.find("]") + 1);
+        function = item.substr(item.find("]") + 2);
+        map[spell] = function;
+        map[function] = spell.substr(1, spell.size() - 2);  // spell 去掉左右中括号 []
+    }
+
+    int n;
+    cin >> n;
+    getchar();  // !!! 吃掉回车
+    string testCase;
+    while (n --) {
+        getline(cin, testCase);
+        if (map[testCase] == "") {
+            cout << "what?" << endl;
+        } else {
+            cout << map[testCase] << endl;
+        }
+    }
+    return 0;
+}
